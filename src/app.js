bf2917fd4823e6195ccae0c8cd11cb2a7e7ae8bf
AldairMoralesAlfaro <ald_brake@hotmail.com>
2025-07-06 04:56:08 +0000

    se corrigio math.random por Math.random

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let who = ['my cat', 'my father', 'the dog', 'the carter'];
+let action = ['ate', 'broke', 'crushed', 'burned'];
+let what = ['my homework', 'my car', 'my home', 'my motorcycle'];
+let when = ['before the class', 'in the night', 'yesterday', 'in the day'];
 
+const pickRandomElementOf = list => list[Math.floor(Math.random() * list.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+    return `${pickRandomElementOf(who)} ${pickRandomElementOf(action)} ${pickRandomElementOf(what)} ${pickRandomElementOf(when)}.`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+    document.getElementById("excuse").innerHTML = generateExcuse();
+    
 };
